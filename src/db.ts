465399d8a4e0dfae924b628323bfadfd9cb21692
anamul01988 <anamulhaque0198877@gmail.com>
2024-03-23 15:53:11 +0600

    backend setup initialized and user created

--- src/db.ts
+++ src/db.ts
@@ -1,17 +1,18 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb+srv://omkhetwal:qfxoQkUzbzQWaoMo@youtube.lothdki.mongodb.net/blossom-app?retryWrites=true&w=majority"
-    )
+      // "mongodb+srv://omkhetwal:qfxoQkUzbzQWaoMo@youtube.lothdki.mongodb.net/blossom-app?retryWrites=true&w=majority"
+      "mongodb+srv://anamulhaque01827:EefiT6pfMKyoTvRg@cluster0.q8kyamj.mongodb.net/todo-app?retryWrites=true&w=majority"
+    );
     if (connection) {
-      console.log("Connection established")
+      console.log("Connection established");
     }
   } catch (error) {
-    console.log("error in connectToDatabase", error)
-    throw error
+    console.log("error in connectToDatabase", error);
+    throw error;
   }
-}
+};
 
-export default connectToDatabase
+export default connectToDatabase;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,10 @@
-import express from "express"
-import { createUser, loginUser } from "../controllers/user.controller"
+import express from "express";
+// import { createUser, loginUser } from "../controllers/user.controller"
+import { createUser } from "../controllers/user.controller";
 
-const userRoutes = express.Router()
+const userRoutes = express.Router();
 
-userRoutes.route("/create").post(createUser)
-userRoutes.route("/login").post(loginUser)
+userRoutes.route("/create").post(createUser);
+// userRoutes.route("/login").post(loginUser);
 
-export default userRoutes
+export default userRoutes;
